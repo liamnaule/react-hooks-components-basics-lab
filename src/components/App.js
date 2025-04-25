16240a6f62880ead743718be407ff00084c91ba7
liamnaule <liamnaule@gmail.com>
2025-04-25 09:28:43 +0300

    completed components basic lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,27 @@
 import React from "react";
 
 function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
+  return <nav>NavBar</nav>;
 }
 
 function Home() {
+  return <div id="home">Home</div>;
+}
+
+function About() {
   return (
-    <div id="home">
-      <h1>Home</h1>
+    <div id="about">
+      <h2>About</h2>
     </div>
   );
 }
-
-{/* write an <About> component here */}
-
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
